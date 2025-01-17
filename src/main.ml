b31a48f7778d562a25e4f655c84efcdaa533db2f
Ivan Nazarov <ivannazarov@carbontec.io>
2025-01-17 00:10:11 -0800

    Implement environment model

--- src/main.ml
+++ src/main.ml
@@ -1,6 +1,19 @@
 open Ast
 
-(* [parse s] parses [s] into an AST. *)
+(** [Env] is a module to help with environments, which are maps that have strings as keys*)
+module Env = Map.Make(String)
+
+let empty_env = Env.empty
+
+(** [env] is the type of an environment, which maps a string to a value*)
+type env = value Env.t
+
+(** [value] is the type of a value *)
+and value =
+  | VInt of int
+  | VBool of bool
+
+(** [parse s] parses [s] into an AST. *)
 let parse (s : string) : expr =
   let lexbuf = Lexing.from_string s in
   let ast = Parser.prog Lexer.read lexbuf in
@@ -47,33 +60,46 @@ let rec eval_small (e : expr) : expr =
   if is_value e then e
   else e |> step |> eval_small
 
-(** [eval_big e] is the [e ==> v] relation. *)
-let rec eval_big (e : expr) : expr = match e with
-  | Int _ | Bool _ -> e
-  | Var _ -> failwith "Unbound variable"
-  | Binop (bop, e1, e2) -> eval_bop bop e1 e2
-  | Let (x, e1, e2) -> subst e2 (eval_big e1) x |> eval_big
-  | If (e1, e2, e3) -> eval_if e1 e2 e3
+(** [eval_big env e] is the [v] such that [<env, e> ==> v]. *)
+let rec eval_big (env: env) (e : expr) : value = match e with
+  | Int i -> VInt i 
+  | Bool b -> VBool b
+  | Var x -> eval_var env x
+  | Binop (bop, e1, e2) -> eval_bop env bop e1 e2
+  | Let (x, e1, e2) -> eval_let env x e1 e2
+  | If (e1, e2, e3) -> eval_if env e1 e2 e3
 
-(** [eval_bop bop e1 e2] is the [e] such that [e1 bop e2 ==> e]. *)
-and eval_bop bop e1 e2 = match bop, eval_big e1, eval_big e2 with
-  | Add, Int a, Int b -> Int (a + b)
-  | Mult, Int a, Int b -> Int (a * b)
-  | Leq, Int a, Int b -> Bool (a <= b)
+
+(** [eval_var env x] is the [v] such that [<env, x> ==> v]. *)
+and eval_var env x =
+  try Env.find x env
+  with Not_found -> failwith "Unbound variable"
+
+(** [eval_bop env bop e1 e2] is the [v] such that [<env, e1 bop e2> ==> v]. *)
+and eval_bop env bop e1 e2 = match bop, eval_big env e1, eval_big env e2 with
+  | Add, VInt a, VInt b -> VInt (a + b)
+  | Mult, VInt a, VInt b -> VInt (a * b)
+  | Leq, VInt a, VInt b -> VBool (a <= b)
   | _ -> failwith "Operator and operand type are mismatch"
 
-and eval_if e1 e2 e3 = match eval_big e1 with
-| Bool true -> eval_big e2
-| Bool false -> eval_big e3
+(** [eval_let env x e1 e2] is the [v] such that [<env, let x = e1 in e2> ==> v]. *)
+and eval_let env x e1 e2 =
+  let v1 = eval_big env e1 in
+  let env' = Env.add x v1 env in
+  eval_big env' e2
+
+(** [eval_if env e1 e2 e3] is the [v] such that [<env, if e1 then e2 else e3> ==> v]. *)
+and eval_if env e1 e2 e3 = match eval_big env e1 with
+| VBool true -> eval_big env e2
+| VBool false -> eval_big env e3
 | _ -> failwith "Guard type must be bool"
 
-(** [string_of_val e] converts [e] to a string. Requires: [e] is a value *)
-let string_of_val (e : expr) : string =
-  match e with
-  | Int i -> string_of_int i
-  | Bool b -> string_of_bool b
-  | _ -> failwith "precondition violated"
+(** [string_of_val v] converts [v] to a string *)
+let string_of_val (v : value) : string =
+  match v with
+  | VInt i -> string_of_int i
+  | VBool b -> string_of_bool b
 
 (** [interp s] interprets [s] by lexing and parsing it, evaluating it, and converting the result to a string *)
 let interp_big (s : string) : string =
-	s |> parse |> eval_big |> string_of_val
+	s |> parse |> eval_big empty_env |> string_of_val
--- test/main.ml
+++ test/main.ml
@@ -5,11 +5,11 @@ open Main
 
 (** [make_i n i s] makes an OUnit test named [n] that expects [s] to evalute to [Int i]. *)
 let make_i n i s =
-  [n >:: (fun _ -> assert_equal (string_of_int i) (interp s))]
+  [n >:: (fun _ -> assert_equal (string_of_int i) (interp_big s))]
 
 (** [make_b n b s] makes an OUnit test named [n] that expects [s] to evalute to [Bool b]. *)
 let make_b n b s =
-  [n >:: (fun _ -> assert_equal (string_of_bool b) (interp s))]
+  [n >:: (fun _ -> assert_equal (string_of_bool b) (interp_big s))]
 
 let tests = [
   make_i "int" 22 "22";
@@ -21,18 +21,18 @@ let tests = [
   make_i "adds" 22 "(10+1)+(5+6)";
   make_b "true" true "true";
   make_b "leq" true "1<=1";
-  (* make_i "let" 22 "let x=22 in x";
+  make_i "let" 22 "let x=22 in x";
   make_i "lets" 22 "let x = 0 in let x = 22 in x";
   make_i "if1" 22 "if true then 22 else 0";
   make_i "if2" 22 "if 1+2 <= 3+4 then 22 else 0";
   make_i "if3" 22 "if 1+2 <= 3*4 then let x = 22 in x else 0";
   make_i "letif" 22 "let x = 1+2 <= 3*4 in if x then 22 else 0";
-  make_t "ty plus" bop_err "1 + true";
+  (*make_t "ty plus" bop_err "1 + true";
   make_t "ty mult" bop_err "1 * false";
   make_t "ty leq" bop_err "true <= 1";
   make_t "if guard" if_guard_err "if 1 then 2 else 3";
   make_t "if branch" if_branch_err "if true then 2 else false";
-  make_t "unbound" unbound_var_err "x"; *)
+  make_t "unbound" unbound_var_err "x";*)
 ]
 
 let _ = run_test_tt_main ("suite" >::: List.flatten tests)
