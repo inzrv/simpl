452b5272fc363a8c62f63118ca645d2b8fd9704e
Ivan Nazarov <ivannazarov@carbontec.io>
2025-01-22 17:44:24 +0300

    Test 3

--- src/main.ml
+++ src/main.ml
@@ -103,3 +103,4 @@ let string_of_val (v : value) : string =
 (** [interp s] interprets [s] by lexing and parsing it, evaluating it, and converting the result to a string *)
 let interp_big (s : string) : string =
 	s |> parse |> eval_big empty_env |> string_of_val
+
